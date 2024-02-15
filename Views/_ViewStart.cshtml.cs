ba0ed42998daa278f1eae36028f30ba35598daba
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2024-02-15 15:09:39 +0300

    project files created

---
+++ Views/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MeetingApp.Views
+{
+    public class _VievStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
