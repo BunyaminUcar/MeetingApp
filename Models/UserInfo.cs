6bf3892f8ee65958907beded7c5d9236746c8129
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2024-02-20 00:31:35 +0300

    Create UserInfo.cs

---
+++ Models/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace MeetingApp.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "İsim bilgisi gereklidir!")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Lütfen telefon numaranızı giriniz!")]
+        public string? Phone { get; set; }
+        [Required(ErrorMessage = "Email bilgisi gereklidir!")]
+        public string? Email { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
